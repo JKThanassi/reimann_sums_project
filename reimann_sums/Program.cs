878aa4399dcf55885af010b758a344aac32c319b
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2016-11-26 14:30:07 -0500

    first working version

--- reimann_sums/Program.cs
+++ reimann_sums/Program.cs
@@ -8,8 +8,45 @@ namespace reimann_sums
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
+        //}
+
+        public static double rightSum(int start, int stop, int n, Func<double,double> f) {
+            double width = (stop - start) / n;
+            double sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                sum += f(start + (i + 1) * width);
+            }
+
+            return width * sum;
         }
+
+        public static double leftSum(int start, int stop, int n, Func<double, double> f) {
+            double width = (stop - start) / n;
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += f(start + (i * width));
+            }
+
+            return width * sum;
+        }
+
+        public static double midSum(int start, int stop, int n, Func<double, double> f)
+        {
+            double width = (stop - start) / n;
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += f(start + ((i + .5) * width));
+            }
+
+            return width * sum;
+        }
+
     }
 }
---
+++ reimann_sums/test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reimann_sums
+{
+    class test_sums
+    {
+        static void Main(String[] args) {
+            Console.WriteLine("Left handed sum: " + Program.leftSum(0,3,3,testFunc));
+            Console.WriteLine();
+            Console.WriteLine("midpt sum: " + Program.midSum(0, 3, 3, testFunc));
+            Console.WriteLine();
+            Console.WriteLine("right handed sum: " + Program.rightSum(0, 3, 3, testFunc));
+
+            Console.ReadLine();
+
+        }
+
+        public static double testFunc(double x) {
+            return Math.Pow(x, 2) + 4 * x + 3;
+        }
+    }
+}
